6367ea713109806ac5a02ec46027d867b291aa16
Baidaa Alkhalaf <135625786+Baidaa1412@users.noreply.github.com>
2023-10-07 22:12:06 +0300

    userprofile

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import UserProfile from './components/Profile/UserProfile';
+import Landingpage from './components/auth/Landingpage';
 
 function App() {
   return (
@@ -10,6 +11,9 @@ function App() {
       <Routes>
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
+        <Route path="/UserProfile" element={<UserProfile />} />
+        <Route path="/" element={<Landingpage />} />
+
       </Routes>
     </BrowserRouter>
   );
